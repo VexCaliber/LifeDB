88540404175db3b280b7ee6eb7a99984bbd79d37
Michael Trzeciakiewicz <114900847+VexCaliber@users.noreply.github.com>
2023-01-15 01:24:40 -0500

    well...rowb won't work...

--- Resources/Code/TableViewController.cs
+++ Resources/Code/TableViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -8,6 +9,7 @@ using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
 using System.Xml;
+using System.Xml.Serialization;
 
 namespace LifeDB.Resources.Code
 {
@@ -18,120 +20,54 @@ namespace LifeDB.Resources.Code
     {
 
         private static Table table;
-
-        //RowGroupCollection contains the rows[]
-        private static TableRowGroupCollection rowGroups;
-
-        //Contains the columns[]
-        private static TableColumnCollection tableColumns;
         
-        //+++We could get the table, and skip init for columns and header row (just below me) and go one by one...ignore the below.
-        //---We need to init() the columns and header row and inject the values 1 by 1 into the document (edit: this edit will be required for extension:::
-        //for making this extensible, you'll need to name the flow doc in the ui, pass that instead of the table,
-        //and build out a new table based on either user input or a string of fields from an outside db file)
-
-        // 0 = Header Row
-        // 1 = First Row
-        private static int currentRow;
+        private static TableRowGroupCollection rowGroups;
+        private static int currentRow;                   
+        
+        private static TableColumnCollection tableColumns;
         private static int currentColumn;
 
-        //pretend this doesn't exist yet...I've been down the rabbit hole going over every level in the intensely too deep bs for the table.
-        //I could just be an idiot...could just do it array style...it is what it is...not set on strat yet...
-        
-
-        private static void AddRow()
-        {
-            //var style = Application.Current.MainWindow.Resources["ResDef.xaml"];
-            //var styles = Application.Current.Resources["ResDef.xaml"];
-            //var styl = Application.Current.Resources.FindName("RowA");
+        private static TableRow rowTemplateA = (TableRow)Application.Current.Resources["RowA"];
+        private static TableRow rowTemplateB = (TableRow)Application.Current.Resources["RowB"];
 
 
-            //var t = Application.Current.FindResource("ResDef.xaml");
-            //var m = t.GetType().FindMembers["RowA"];
-            //var rd = new System.Windows.ResourceDictionary();
-            //var ke = rd.Source = new Uri("ResDef.xaml");
-            //var kd[0];
-
-            TableRow rowTemplateA = (TableRow)Application.Current.Resources["RowA"]; // YES!
-            TableRow rowTemplateB = (TableRow)Application.Current.Resources["RowB"]; // Why the hell was this so hard? 
-
-            // #5
-            // var r = new TableRow();
-            //r.FindResource(resourceKey: "RowA");
-            //r.SetResourceReference(DependencyProperty.Register("RowA", Application.Current.Resources.FindName("RowA").GetType,r.GetType()),r);
-            table.RowGroups[0].Rows.Add(new TableRow());//
-            table.RowGroups[0].Rows.Add(rowTemplateA);
-            table.RowGroups[0].Rows.Add(rowTemplateB);
-            /*
-             * 
-             * Ok...so now we've left only the columns and the header row.
-             * Now...there is no limit on the row length...it'll grow infinitely
-             * We'll go loose on te columns, for future need in extension, and
-             * then, all we have to do is get it to create the new row based on the template
-             * 
-             */
-
-            if (currentRow % 2 == 0)
-            {
-
-                var l = table.RowGroups[0].Rows[currentRow];
-                l.Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
-                l.Cells.Add(new TableCell(new Paragraph(new Run("Product2"))));
-                l.Cells.Add(new TableCell(new Paragraph(new Run("Product3"))));
-
-            }
-            else
-            {
-                var l = table.RowGroups[0].Rows[currentRow];
-                l.Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
-                l.Cells.Add(new TableCell(new Paragraph(new Run("Product2"))));
-                l.Cells.Add(new TableCell(new Paragraph(new Run("Product3"))));
-            }
-
-            //Thread.Sleep(10000);
-            //table = new Table();
-            //For new tables, we need to unload any decl. in the xaml
-
-        }
-
-        private static void GenerateRow()
+        private static void GenerateRow(List<String> values)
         {
 
-            TableRow rowTemplateA = (TableRow)Application.Current.Resources["RowA"];
-            TableRow rowTemplateB = (TableRow)Application.Current.Resources["RowB"];
 
-            // var l = table.RowGroups[0].Rows[currentRow];
-            // l.Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
-
-            //table.RowGroups[0].Rows.Add(new TableRow());
-            //table.RowGroups[0].Rows.Add(rowTemplateA);
-            //table.RowGroups[0].Rows.Add(rowTemplateB);
-
-            if (currentRow % 2 == 0)
+            if (!(currentRow % 2 == 0))//currentRow % 2 == 0
             {
+
                 table.RowGroups[0].Rows.Add(rowTemplateA);
-                table.RowGroups[0].Rows.Add(rowTemplateB);
-                var x = table.RowGroups[5].Rows[currentRow].Cells.ToArray();
-                var y = x[0];
-                var z = y.Blocks;
-                var a = z.FirstBlock;
-                //var b = a.
+                currentRow++;
+                var cells = table.RowGroups[0].Rows[currentRow].Cells.ToList();
                 
-                ///I swear...this is just unreasonable...i know it's for flow, but there really needs to be a better abstraction.
-                ///on the bright side...we can add a row now!  and it's a row on command
-                ///but I do wonder why the 2nd one won't come up...possibly just brainmelted, but
-                ///it may be a name conflict with the xaml...but it's a key, so hopefully it's not that...idk though
 
-                //table.RowGroups[0].Rows[currentRow].Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
-                //table.RowGroups[0].Rows[currentRow].Cells.Insert(2,new TableCell(new Paragraph(new Run("Product"))));
-                //table.RowGroups[0].Rows[currentRow].Cells.Prepend(new TableCell(new Paragraph(new Run("Product"))));
+                for(int i = 0; i < cells.Count-1; i++)
+                {
+                    //Found the answer...but god this is still awful...I'll do something about it...
+                    table.RowGroups[0].Rows[currentRow].Cells[i].Blocks.Clear();
+                    table.RowGroups[0].Rows[currentRow].Cells[i].Blocks.Add(new Paragraph(new Run(values[i])));
+                }
 
             }
             else
             {
+
                 table.RowGroups[0].Rows.Add(rowTemplateB);
-                table.RowGroups[0].Rows[currentRow].Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
+                
+                currentRow++;
+                var cells = table.RowGroups[0].Rows[currentRow].Cells.ToList();
+
+                for (int i = 0; i < cells.Count - 1; i++)
+                {
+                    //Found the answer...but god this is still awful...I'll do something about it...
+                    table.RowGroups[0].Rows[currentRow].Cells[i].Blocks.Clear();
+                    table.RowGroups[0].Rows[currentRow].Cells[i].Blocks.Add(new Paragraph(new Run(values[i])));
+                }
+
             }
+
 
         }
 
@@ -143,25 +79,41 @@ namespace LifeDB.Resources.Code
             rowGroups = t.RowGroups;
             tableColumns = t.Columns;
 
-            currentRow = 1;
+            currentRow = 0;
             currentColumn = 0;
 
-            GenerateRow();
-           
+            GenerateRow(new List<string> { "a", "b", "c", "d", "e", "f", });
+            GenerateRow(new List<string> { "b", "b", "c", "d", "e", "f", });
+            GenerateRow(new List<string> { "c", "b", "c", "d", "e", "f", });
+
 
         }
 
     }
 
-/*                var x = table.RowGroups[0];
-                var y = table.Columns[columnCount];
-                var k = table.RowGroups[0].Rows[0];
 
-                var o = table.RowGroups[0].Rows[1];
-                    o.Cells[1].Add(new TableCell(new Paragraph(new Run("TEST"))));
 
-                var l = table.RowGroups[0].Rows[rowCount];
-                l.Cells[rowCount].Add(new TableCell(new Paragraph(new Run("Product"))));*/
+
+
+
+
+
+
+
+
+
+
+
+
+    /*                var x = table.RowGroups[0];
+                    var y = table.Columns[columnCount];
+                    var k = table.RowGroups[0].Rows[0];
+
+                    var o = table.RowGroups[0].Rows[1];
+                        o.Cells[1].Add(new TableCell(new Paragraph(new Run("TEST"))));
+
+                    var l = table.RowGroups[0].Rows[rowCount];
+                    l.Cells[rowCount].Add(new TableCell(new Paragraph(new Run("Product"))));*/
 
     // List<TableCell<Paragraph<Run>>> l = new List<TableCell<Paragraph<Run>>>();
 
@@ -195,6 +147,84 @@ namespace LifeDB.Resources.Code
      * 
      */
 
+    // var l = table.RowGroups[0].Rows[currentRow];
+    // l.Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
 
+    //table.RowGroups[0].Rows.Add(new TableRow());
+    //table.RowGroups[0].Rows.Add(rowTemplateA);
+    //table.RowGroups[0].Rows.Add(rowTemplateB);
+
+    //pretend this doesn't exist yet...I've been down the rabbit hole going over every level in the intensely too deep bs for the table.
+    //I could just be an idiot...could just do it array style...it is what it is...not set on strat yet...
+
+    //+++We could get the table, and skip init for columns and header row (just below me) and go one by one...ignore the below.
+    //---We need to init() the columns and header row and inject the values 1 by 1 into the document (edit: this edit will be required for extension:::
+    //for making this extensible, you'll need to name the flow doc in the ui, pass that instead of the table,
+    //and build out a new table based on either user input or a string of fields from an outside db file)
+
+    /*
+private static void AddRow()
+{
+    //var style = Application.Current.MainWindow.Resources["ResDef.xaml"];
+    //var styles = Application.Current.Resources["ResDef.xaml"];
+    //var styl = Application.Current.Resources.FindName("RowA");
+
+
+    //var t = Application.Current.FindResource("ResDef.xaml");
+    //var m = t.GetType().FindMembers["RowA"];
+    //var rd = new System.Windows.ResourceDictionary();
+    //var ke = rd.Source = new Uri("ResDef.xaml");
+    //var kd[0];
+
+    TableRow rowTemplateA = (TableRow)Application.Current.Resources["RowA"]; // YES!
+    TableRow rowTemplateB = (TableRow)Application.Current.Resources["RowB"]; // Why the hell was this so hard? 
+
+    // #5
+    // var r = new TableRow();
+    //r.FindResource(resourceKey: "RowA");
+    //r.SetResourceReference(DependencyProperty.Register("RowA", Application.Current.Resources.FindName("RowA").GetType,r.GetType()),r);
+    table.RowGroups[0].Rows.Add(new TableRow());//
+    table.RowGroups[0].Rows.Add(rowTemplateA);
+    table.RowGroups[0].Rows.Add(rowTemplateB);
+    /*
+     * 
+     * Ok...so now we've left only the columns and the header row.
+     * Now...there is no limit on the row length...it'll grow infinitely
+     * We'll go loose on te columns, for future need in extension, and
+     * then, all we have to do is get it to create the new row based on the template
+     * 
+     *
+
+    if (currentRow % 2 == 0)
+    {
+
+        var l = table.RowGroups[0].Rows[currentRow];
+        l.Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
+        l.Cells.Add(new TableCell(new Paragraph(new Run("Product2"))));
+        l.Cells.Add(new TableCell(new Paragraph(new Run("Product3"))));
+
+    }
+    else
+    {
+        var l = table.RowGroups[0].Rows[currentRow];
+        l.Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
+        l.Cells.Add(new TableCell(new Paragraph(new Run("Product2"))));
+        l.Cells.Add(new TableCell(new Paragraph(new Run("Product3"))));
+    }
+
+    //Thread.Sleep(10000);
+    //table = new Table();
+    //For new tables, we need to unload any decl. in the xaml
+
+} */
+
+
+    // Clear the content
+    //cell.Blocks.Clear();
+
+    // Add some text
+    //cell.Blocks.Add(new Paragraph(new Run("Hello world")));
+
+    //table.RowGroups[0].Rows[currentRow].Cells.Add(new TableCell(new Paragraph(new Run("Product"))));
 
 }
