6606ac453f6315f525ca7e732769dd09415ca9f9
Michael Trzeciakiewicz <114900847+VexCaliber@users.noreply.github.com>
2023-02-17 04:10:11 -0500

    small changes...

---
+++ Resources/Code/IntelViewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeDB.Resources.Code
+{
+    internal class IntelViewController
+    {
+
+
+
+        public static void Init()
+        {
+
+        }
+
+    }
+}
