a215698a40a65554b5a99d407e2658df22dd2c41
Michael Trzeciakiewicz <114900847+VexCaliber@users.noreply.github.com>
2023-02-25 03:49:02 -0500

    Minor Additions...

--- Resources/Code/IntelViewController.cs
+++ Resources/Code/IntelViewController.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace LifeDB.Resources.Code
 {
     internal class IntelViewController
     {
 
-        private static StackPanel snippetPanel;
-        private static StackPanel summaryPanel;
+        private static StackPanel? snippetPanel;
+        private static StackPanel? summaryPanel;
 
-        private static SQLiteDataReader data;
+        private static SQLiteDataReader? data;
 
         public static void Init(StackPanel snippets, StackPanel summary)
         {
@@ -54,12 +56,26 @@ namespace LifeDB.Resources.Code
             return data; 
         }
 
+        //==========Command==========//
+
+        public static void RunStats()
+        {
+
+            getData();
+
+            if (data == null) return;
+
+            ScanTotalEntries();
+            ScanNames();
+            ScanEaches();
+
+        }
 
         //==========Scanners==========//
 
-        //| id | item_name | item_quantity | item_category | added | expires | limit |
+        //|0 id |1 item_name |2 item_quantity |3 item_category |4 added |5 expires |6 limit |
 
-        private static void TotalItems()
+        private static void ScanTotalEntries()
         {
             
             int count = 0;
@@ -69,8 +85,8 @@ namespace LifeDB.Resources.Code
                 count = (Int32)data.GetInt64(0);
             }
 
-            WriteToSnippets("There are " + count + " items in the table.");
-            WriteToSummary("Items("+count+")");
+            WriteToSnippets("There are " + count + " entries in the table.");
+            WriteToSummary("Entries("+count+")");
             
         }
 
@@ -122,8 +138,41 @@ namespace LifeDB.Resources.Code
         private static void ScanEaches()
         {
 
+            int eachesTotal = 0;
+
+            while (data.Read()) eachesTotal += data.GetInt32(2);
+
+            WriteToSnippets("Total Amount of item eaches comes to a total of: "+eachesTotal);
+            WriteToSummary("Total Eaches = "+eachesTotal);
+
         }
 
+        //shaky
+        private static void ScanDates()
+        {
+
+            Func<String, String> normalize = (i) => { return TableViewController.DateNormalizer(i); };
+
+            String xDate;
+            String yDate;
+
+            while (data.Read())
+            {
+                xDate = normalize(data.GetValue(4).ToString()); 
+                yDate = normalize(data.GetValue(5).ToString());
+
+                if (xDate.Length != yDate.Length) continue; //catch-all for non-comparables - remember, xDate will always be defaulted to current date whilst yDate will default to -1
+
+                //kk, didn't do much today, but ksp2 came out officially early access so...considering I got turned down by yet another tech job I'm more than qual for...
+                //I'ma go play rocket man ^.^ for the rest of the day.
+
+                //var preform = reader.GetValue(i);
+                //var d = preform.ToString();
+                //values.Add(DateNormalizer(d));
+
+            }
+
+        }
 
 
     }
